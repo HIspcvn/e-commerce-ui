7749df0fb6bc0a61d18b8f4b3c8bb6b13de51841
SPCCPS <23001942@hus.edu.vn>
2025-10-03 22:44:12 +0700

    product detail page p2

--- src/pages/ProductDetailPage/ProductDetail.js
+++ src/pages/ProductDetailPage/ProductDetail.js
@@ -1,10 +1,41 @@
-import react, { use } from "react";
-import { useParams } from "react-router-dom";
+import react, { use, useState } from "react";
+import { useLoaderData, useParams } from "react-router-dom";
 
 const ProductDetail = () => {
-    const { productId } = useParams()
+    const { product } = useLoaderData()
+    console.log(product)
+    const [image, setImage] = useState(product.thumbnail)
+    console.log(image)
+
+
     return (
-        <div>ProductDetail {productId}</div>
+        <div className="flex flex-col lg:flex-row md:flex-row p-10 h-[320px]">
+            <div className="w-[100%] lg:w-[50%] md:w-[40%]">
+                {/*Image*/}
+                <div className="flex flex-col md:flex-row">
+                    <div className="w-[100%] md:w-[30%] justify-center h-[40px] md:h-[420px]">
+                        {/* Stack images */}
+                        <div className="flex flex-row md:flex-col justify-center h-full">
+                            {
+                                product.images.map((item, index) => (
+                                    <button onClick={() => setImage(item)} className="border rounded-lg w-fit p-2 mb-2">
+                                        <img src={item} className="h-48[px]  w-[48px] 
+                                        bg-cover bg-center hover:scale-105" alt={'sample' + index} />
+                                    </button>
+                                ))
+                            }
+                        </div>
+                    </div>
+                    <div className="flex w-full md:w-[80%] justify-center md:pt-0 pt-10">
+                        <img src={image} className={`h-full w-full max-h-[520px] object-cover 
+                            border rounded-lg cursor-pointer block`} alt={product.title} />
+                    </div>
+                </div>
+            </div>
+            <div className="w-[60%]">
+
+            </div>
+        </div>
     )
 }
 
--- src/routes/products.js
+++ src/routes/products.js
@@ -2,6 +2,6 @@ import content from '../data/content.json'
 
 
 export const loadProductById = ({ params }) => {
-    const product = content.products.find((product) => product.id === params.productId.toString())
+    const product = content.products.find((product) => product.id.toString() === params.productId.toString())
     return { product }
 }
