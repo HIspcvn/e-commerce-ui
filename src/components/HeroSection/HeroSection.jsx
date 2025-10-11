95f58f56632d76af26ba0787f9e7b2e023cbdd9e
SPCCPS <23001942@hus.edu.vn>
2025-10-11 20:05:49 +0700

    add img

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import HeroImg from '../../assets/image/hero-img.png';
+import HeroImg from '../../assets/image/heroend.png';
 
 const HeroSection = () => {
     return (
@@ -10,14 +10,14 @@ const HeroSection = () => {
             <div className="absolute top-22 right-0 left-0 ">
                 <main className="px-10 lg:px-24 z-10">
                     <div className="text-left">
-                        <h2 className="text-2xl text-white">Stay Fresh / Wear Better</h2>
-                        <p className="mt-3 text-white sm:max-w-xl text-6xl">
+                        <h2 className="text-2xl text-black">Stay Fresh / Wear Better</h2>
+                        <p className="mt-3 text-black sm:max-w-xl text-6xl">
                             Style For Everyone, Every Day
                         </p>
-                        <p className="mt-3 text-white sm:max-w-xl text-2xl">
+                        <p className="mt-3 text-black sm:max-w-xl text-2xl">
                             casual / modern / fun
                         </p>
-                        <button className="border rounded mt-20 border-black hover:bg-white hover:text-black hover:border-black text-white bg-black w-44 h-12">
+                        <button className="border rounded mt-[20px] border-black hover:bg-white hover:text-black hover:border-black text-white bg-black w-44 h-12">
                             Shop now!
                         </button>
                     </div>
